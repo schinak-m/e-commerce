516638110d8a2ed75b603fdf8e70512fa1396503
Shinak Mohemed <126258374+schinak-m@users.noreply.github.com>
2024-01-30 22:33:39 +0300

    Update axiosClient.js

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-/* const axiosClient = axios.create({
-    baseURL: 'https://fakestoreapi.com/'
-})
- */
 const axiosClient = axios.create({
     baseURL: 'https://dummyjson.com/'
 })
