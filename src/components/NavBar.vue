5e02497c6147de8159de872db4a1e3cdce2192ef
schinak-m <shinak.x3@gmail.com>
2024-02-09 17:00:20 +0300

    Updated NavBar code

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -33,12 +33,11 @@
           <li class="nav-item">
             <div class="relative inline-block text-left ">
               <div>
-                <button type="button" @click="showDropDown()" 
+                <button type="button" @click="showDropDown()"
                   class="inline-flex w-full justify-center gap-x-1 px-3 pt-1.5 items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
                   id="menu-button" aria-expanded="true" aria-haspopup="true">
                   Men/Women
-                  <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                    aria-hidden="true">
+                  <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd"
                       d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
                       clip-rule="evenodd" />
@@ -103,28 +102,19 @@
   </nav>
 </template>
   
-<script>
+<script setup>
 import { RouterLink } from 'vue-router';
 import { ref } from 'vue'
 import SideBar from './SideBar.vue';
 
-export default {
-  setup() {
-    const showMenu = ref(false);
-    const show = ref(false);
-    function toggleNavbar() {
-      showMenu.value = !showMenu.value;
-    }
-    function showDropDown() {
-      show.value = !show.value;
-    }
-    return {
-      showMenu,
-      show,
-      toggleNavbar,
-      showDropDown
-    };
-  },
-  components: { SideBar }
+const showMenu = ref(false);
+const show = ref(false);
+function toggleNavbar() {
+  showMenu.value = !showMenu.value;
 }
+
+function showDropDown() {
+  show.value = !show.value;
+}
+
 </script>
